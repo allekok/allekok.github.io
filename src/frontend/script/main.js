f9fafcf238b8afb733ab3b5a660cf4562ff57d54
Payam Bapiri <payambapiri.97@gmail.com>
2020-01-28 19:29:02 +0330

    main.js: Fix a typo:(9->21)

--- src/frontend/script/main.js
+++ src/frontend/script/main.js
@@ -1,7 +1,7 @@
 /* Night mode */
 const D = new Date;
 const h = D.getHours();
-if((h >= 0 && h <= 5) || h >= 9)
+if((h >= 0 && h <= 5) || h >= 21)
 {
     document.body.style.filter = "invert(1)";
 }
