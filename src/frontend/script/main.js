1379742eedb337f0e155fe08cfc8054bb9dbbb47
Payam Bapiri <payambapiri.97@gmail.com>
2020-02-13 12:10:44 +0330

    Minor improvements + Adding 'README'

--- src/frontend/script/main.js
+++ src/frontend/script/main.js
@@ -1,7 +1,9 @@
 /* Night mode */
-const D = new Date;
-const h = D.getHours();
-if(!(h > 6 && h < 18))
+function dark ()
 {
     document.body.style.filter = "invert(1)";
 }
+const D = new Date;
+const h = D.getHours();
+if(!(h > 6 && h < 18))
+    dark();
